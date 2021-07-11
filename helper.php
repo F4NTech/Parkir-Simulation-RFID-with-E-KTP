619475917821d59c2bd6dfbf22a5c63a4b3becd6
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2021-07-11 18:25:02 +0700

    mem

--- helper.php
+++ helper.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 //WAKTU
 require 'test.php';
 date_default_timezone_set('Asia/Jakarta');
@@ -8,17 +8,19 @@ date_default_timezone_set('Asia/Jakarta');
 
 function INSERT($nama, $nim, $merk, $plat, $jam, $hari)
 {
-	global $koneksi;
-	$sql = "INSERT INTO masuk(nama, nim, merk, plat, tanggal, keluar) VALUES
-	('$nama','$nim','merek', $plat','$jam','$hari')";
+	global $conn;
+	$sql = mysqli_query($conn, "INSERT INTO 
+								masuk(nama, nim, merk, plat, tanggal, keluar) 
+								VALUES
+								('$nama','$nim','$merk','$plat','$jam','$hari')
+						");
 	//mysql_query($koneksi, $sql);
-	if (mysql_query($koneksi, $sql)) {
- 		echo "New record ".$nama[1]." sukses";
-  		echo " ".$jam;
-  		//Hidupkan Lampu ijo
-		} else {
- 			echo "Error: " . $sql . "<br>" . mysqli_error($koneksi);
-		}
+	var_dump($sql);
+	// if (mysqli_query($koneksi, $sql)) {
+	// 	echo "New record " . $nama[1] . " sukses";
+	// 	echo " " . $jam;
+	// 	//Hidupkan Lampu ijo
+	// } else {
+	// 	echo "Error: " . $sql . "<br>" . mysqli_error($koneksi);
+	// }
 }
-
- ?>
--- test.php
+++ test.php
@@ -1,17 +1,14 @@
 <?php
-$username = "oem";
+$servername = "localhost";
+$database = "test";
+$username = "root";
 $password = "";
-$db	= "test";
 
 // untuk tulisan bercetak tebal silakan sesuaikan dengan detail database Anda
 // membuat koneksi
-$koneksi = mysqli_connect('localhost', $username, $password, $db);
-
-
+$conn = mysqli_connect($servername, $username, $password, $database);
 // mengecek koneksi
-// if (!$koneksi) {
-//     die("Koneksi gagal: " . mysqli_connect_error());
-// }
-// echo "Koneksi berhasil";
-// mysqli_close($koneksi);
-?>
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+echo "Koneksi berhasil";

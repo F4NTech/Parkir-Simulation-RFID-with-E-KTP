6318d8c1d33db7cc416a7988d3746d1906dc6c09
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2021-07-14 23:49:41 +0700

    absen masuk

--- helper.php
+++ helper.php
@@ -2,7 +2,7 @@
 //WAKTU
 include 'test.php';
 date_default_timezone_set('Asia/Jakarta');
-//global $koneksi;
+//global $conn;
 
 
 
@@ -10,26 +10,35 @@ function INSERT($nama, $nim, $merk, $plat, $jam, $tanggal, $status)
 {
 
 	global $conn;
-	$sql = mysqli_query($conn, "INSERT INTO 
+
+	//cek data sudah keluar
+	$query = mysqli_query($conn, "SELECT * FROM masuk WHERE nama='$nama' ORDER BY id DESC LIMIT 1");
+	$check = mysqli_fetch_assoc($query);
+
+
+	if ($check["status"] == "masuk") {
+		echo "Status absen hari ini";
+	} else {
+		$sql = mysqli_query($conn, "INSERT INTO 
 			masuk(nama, nim, merk, plat, jam, tanggal, status) 
 			VALUES
 			('$nama','$nim','$merk','$plat','$jam','$tanggal', '$status')
 						");
-	var_dump($sql);
-	if ($sql) {
-	echo "New record " . $nama . " sukses";
-	echo " " . $jam;
-		//Hidupkan Lampu ijo
-	} else {
-		echo "Error: " . $sql . "<br>" . mysqli_error($koneksi);
+		var_dump($sql);
+		if ($sql) {
+			echo "New record " . $nama . " sukses";
+			echo " " . $jam;
+			//Hidupkan Lampu ijo
+		} else {
+			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
 	}
-
 }
 
 function INSERT_IN($nama, $nim, $merk, $plat, $jam, $tanggal, $status)
 {
 	global $conn;
-	$query = mysqli_query($conn,"SELECT * FROM masuk");
+	$query = mysqli_query($conn, "SELECT * FROM masuk");
 
 	foreach ($query as $value) {
 		$last = $value['id'];
@@ -37,7 +46,7 @@ function INSERT_IN($nama, $nim, $merk, $plat, $jam, $tanggal, $status)
 	var_dump($last);
 
 	//cek data sudah keluar
-	$query = mysqli_query($conn,"SELECT * FROM masuk WHERE id = $last AND status = 'keluar' ");
+	$query = mysqli_query($conn, "SELECT * FROM masuk WHERE id = $last AND status = 'keluar' ");
 	$check = mysqli_fetch_row($query);
 	//echo($check[7]);
 	if (!$check) {
@@ -45,28 +54,27 @@ function INSERT_IN($nama, $nim, $merk, $plat, $jam, $tanggal, $status)
 		//echo $last;
 
 
-	}else{
+	} else {
 		$sql = mysqli_query($conn, "INSERT INTO 
 								masuk(nama, nim, merk, plat, jam, tanggal, status) 
 								VALUES
 								('$nama','$nim','$merk','$plat','$jam','$tanggal', '$status')
 						");
-	//var_dump($sql);
-	if ($sql) {
-	echo "New record " . $nama . " sukses";
-	echo " " . $jam;
-		//Hidupkan Lampu ijo
-	} else {
-		echo "Error: " . $sql . "<br>" . mysqli_error($koneksi);
+		//var_dump($sql);
+		if ($sql) {
+			echo "New record " . $nama . " sukses";
+			echo " " . $jam;
+			//Hidupkan Lampu ijo
+		} else {
+			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
 	}
-	}
-
 }
 
 function INSERT_OUT($nama, $nim, $merk, $plat, $jam, $tanggal, $status)
 {
 	global $conn;
-	$query = mysqli_query($conn,"SELECT * FROM masuk");
+	$query = mysqli_query($conn, "SELECT * FROM masuk");
 
 	foreach ($query as $value) {
 		$last = $value['id'];
@@ -74,7 +82,7 @@ function INSERT_OUT($nama, $nim, $merk, $plat, $jam, $tanggal, $status)
 	var_dump($last);
 
 	//cek data sudah keluar
-	$query = mysqli_query($conn,"SELECT * FROM masuk WHERE id = $last AND status = 'masuk' ");
+	$query = mysqli_query($conn, "SELECT * FROM masuk WHERE id = $last AND status = 'masuk' ");
 	var_dump($query);
 	$check = mysqli_fetch_row($query);
 	var_dump($check);
@@ -83,20 +91,19 @@ function INSERT_OUT($nama, $nim, $merk, $plat, $jam, $tanggal, $status)
 		//echo $last;
 
 
-	}else{
+	} else {
 		$sql = mysqli_query($conn, "INSERT INTO 
 								masuk(nama, nim, merk, plat, jam, tanggal, status) 
 								VALUES
 								('$nama','$nim','$merk','$plat','$jam','$tanggal', '$status')
 						");
-	var_dump($sql);
-	if ($sql) {
-	echo "New record " . $nama . " sukses";
-	echo " " . $jam;
-		//Hidupkan Lampu ijo
-	} else {
-		echo "Error: " . $sql . "<br>" . mysqli_error($koneksi);
+		var_dump($sql);
+		if ($sql) {
+			echo "New record " . $nama . " sukses";
+			echo " " . $jam;
+			//Hidupkan Lampu ijo
+		} else {
+			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
 	}
-	}
-
 }
